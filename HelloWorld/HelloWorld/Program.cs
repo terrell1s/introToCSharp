36664604f98f8b866469c4c49065fc9e6a1fc92a
Terrell Springer <twspringer@gmail.com>
2014-06-26 11:03:29 -0400

    Created hello world
    Started the hello world codes also created a variable.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputReadline;
+
+        Console.WriteLine("Hello World");
+        inputReadline = Console.ReadLine();
+        Console.WriteLine(inputReadline);
+        Console.ReadLine();
+           
+        }
+    }
+}
